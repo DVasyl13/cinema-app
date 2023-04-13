482c6013533255623966fef3fd6aae7c6e91ed11
DVasyl13 <vasylldruchok6119@gmail.com>
2023-04-13 13:01:33 +0300

    added login and register logic

--- cinema/src/main/java/com/lab/app/controller/AuthController.java
+++ cinema/src/main/java/com/lab/app/controller/AuthController.java
@@ -3,35 +3,45 @@ package com.lab.app.controller;
 import com.lab.app.dto.NewUserSubmission;
 import com.lab.app.dto.UserSubmission;
 import com.lab.app.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
-    private UserService userService;
+    private final UserService userService;
 
-    @Transactional
     @PostMapping("/login")
-    public boolean login(@RequestParam("user") UserSubmission userSubmission) {
-
-        //userService .....
-
-        return false;
+    public ResponseEntity<String> login(@RequestBody UserSubmission userSubmission, HttpSession session) {
+        if (userService.authenticateUser(userSubmission.email(), userSubmission.password())) {
+            session.setAttribute("email", userSubmission.email()); // може спрацює
+            session.setAttribute("password", userSubmission.password());
+            return new ResponseEntity<>("Вхід успішний", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Такого користувача не існує", HttpStatus.FORBIDDEN);
+        }
     }
 
-    @Transactional
     @PostMapping("/register")
-    public boolean registration(@RequestParam("user") NewUserSubmission userSubmission) {
+    public ResponseEntity<String> registration(@RequestBody NewUserSubmission userSubmission, HttpSession session) {
+        boolean isCreated = userService.saveUser(userSubmission);
 
-        //userService .....
-
-        return false;
+        if (isCreated) {
+            session.setAttribute("email", userSubmission.email()); // може спрацює
+            session.setAttribute("password", userSubmission.password());
+            return new ResponseEntity<>("Аккаунт створено", HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>("Цей email вже зайнято", HttpStatus.FORBIDDEN);
+        }
     }
 }
--- cinema/src/main/java/com/lab/app/service/UserService.java
+++ cinema/src/main/java/com/lab/app/service/UserService.java
@@ -1,5 +1,6 @@
 package com.lab.app.service;
 
+import com.lab.app.dto.NewUserSubmission;
 import com.lab.app.entity.User;
 import com.lab.app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,26 +8,49 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    private BCryptPasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder;
 
 
     @Transactional
-    public void saveUser(User user) {
+    public boolean saveUser(NewUserSubmission newUser) {
+        User user = new User( newUser.name(),
+                            newUser.surname(),
+                            newUser.password(),
+                            newUser.email());
+
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
-        userRepository.save(user);
+
+        if (isEmailAlreadyInUse(user.getEmail())) {
+            return false;
+        }
+        else {
+            userRepository.save(user);
+        }
+        return true;
     }
 
     @Transactional
     public boolean authenticateUser(String email, String password) {
         User user = userRepository.findUserByEmail(email);
         return user != null && passwordEncoder.matches(password, user.getPassword());
+    }
+
+    /**
+     * @return true if email is already in use, otherwise false
+     */
+    @Transactional
+    protected boolean isEmailAlreadyInUse(String email) {
+        return Optional.ofNullable(userRepository.findUserByEmail(email))
+                .isPresent();
     }
 
 }
