978e2c7be639f7e5c9e1bcb821b0dfed0e51f49c
andriymarych <andriumarich@gmail.com>
2023-04-20 21:24:25 +0300

    Refactored project files, added close buttons and login form animation

--- cinema/src/main/resources/static/js/script.js
+++ cinema/src/main/resources/static/js/script.js
@@ -1,24 +1,49 @@
-var loginBtn = document.getElementById("loginBtn");
-var shadowBG = document.getElementById("backgroundPopup");
-var regBtn = document.getElementById("regPopupBtn");
+const loginFormPopUpButton = document.getElementById("login-form-button");
 
+const loginBtn = document.getElementById("main-login-button");
+const regBtn = document.getElementById("registration-popup-button");
+
+const shadowBG = document.getElementById("background-popup");
+
+const closeLoginFormBtn  = document.getElementById("close-login-button");
+const closeRegistrationFormBtn  = document.getElementById("close-registration-button");
 loginBtn.addEventListener('click', ()=>{
-    document.querySelector(".loginPopup").style.display = "block";
+    document.querySelector(".login-popup").classList.add("active");
+    document.querySelector(".registration-popup").classList.remove("active");
     controlDisapearingBG(1);
 })
 
 regBtn.addEventListener('click', ()=>{
-    document.querySelector(".loginPopup").style.display = "none";
-    document.querySelector(".regPopup").style.display = "block";
+    document.querySelector(".login-popup").classList.remove("active");
+    document.querySelector(".registration-popup").classList.add("active");
 })
-
+closeLoginFormBtn.addEventListener('click', ()=>{
+    document.querySelector(".login-popup").classList.remove("active");
+    document.querySelector(".registration-popup").classList.remove("active");
+    controlDisapearingBG(0)
+})
+closeRegistrationFormBtn.addEventListener('click', ()=>{
+    document.querySelector(".login-popup").classList.remove("active");
+    document.querySelector(".registration-popup").classList.remove("active");
+    controlDisapearingBG(0)
+})
 shadowBG.addEventListener('click', ()=>{
-    document.querySelector(".loginPopup").style.display = "none";
-    document.querySelector(".regPopup").style.display = "none";
-    controlDisapearingBG(0);
+    document.querySelector(".login-popup").classList.remove("active");
+    document.querySelector(".registration-popup").classList.remove("active");
+    controlDisapearingBG(0)
 })
+loginFormPopUpButton.addEventListener('click', ()=>{
+    document.querySelector(".login-popup").classList.add("active");
+    document.querySelector(".registration-popup").classList.remove("active");
+    controlDisapearingBG(1);
+})
+const controlDisapearingBG = (flag) =>{
+    (flag)
+        ? shadowBG.style.visibility = "visible"
+        : shadowBG.style.visibility = "hidden";
+}
 
-const submitContact = (event) => {
+const resisterNewUser = (event) => {
 
     let feadbackForm = event.target;
 
@@ -27,21 +52,48 @@ const submitContact = (event) => {
     createNewUser(feadBack);
 }
 
-const controlDisapearingBG = (flag) =>{
-    (flag) 
-    ? shadowBG.style.display = "block" 
-    : shadowBG.style.display = "none";
-}
-
-const createNewUser = (data) => {    
+const createNewUser = (data) => {
 
     console.log(data);
 
-    if (data["Password"] != data["DupPassword"]) {
+    if (data["password"] !== data["repeat-password"]) {
         return;
     }
 
-    fetch('/', {
+    fetch('/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+const loginUser = (event) => {
+
+    let feadbackForm = event.target;
+
+    let feadBack = Object.fromEntries(new FormData(feadbackForm));
+
+    verifyUser(feadBack);
+}
+
+const verifyUser = (data) => {
+    console.log(data);
+
+    fetch('/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
